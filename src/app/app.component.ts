1e92ba55c39679f8a309b2dac69a759a4db67ff6
prashanthreddyt <prashanth.talakanti@sparity.com>
2020-02-24 11:57:03 +0530

    pass data with two functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit{
     });
   }
 
-  paymentform(data) {
+  paymentform() {
     var authData = {};
         authData["clientKey"] = "7cj5EL8Dg4bdJqtXsygQZKRxNXjyQ8m4P4VVQgqg53aYSQ8RmvM7G4wMc9qN7Gvv";
         authData["apiLoginID"] = "7MzB2j6t";
@@ -72,30 +72,28 @@ export class AppComponent implements OnInit{
             (<HTMLInputElement>document.getElementById("year")).value = "";
             (<HTMLInputElement>document.getElementById("code")).value = "";
             console.log(response.opaqueData.dataValue);
-            this.postpaymentdata1(data);
             
         }
     }
 }
 
-postpaymentdata1(data) {
-  
-
-  setTimeout(function(){
-    if(data["dataValue"] != undefined){
-      var paymentprocessinfo = {
-        "dataValue" : data["dataValue"],
-        "dataDescriptor" : data["dataDescriptor"],
-        "amount" : data["amount"],
-        "address_zip" : data["address_zip"]
-      }
-      console.log(data);
-      console.log(paymentprocessinfo);
-      this.http.post(this.url+"/paymentforconsultaion/5e43a999feb8ea2affebb7f6",paymentprocessinfo)
-        .subscribe(paymentres => {
-        });
+postpaymentdata(data) {
+  if(data["dataValue"] != undefined){
+    var paymentprocessinfo = {
+      "dataValue" : data["dataValue"],
+      "dataDescriptor" : data["dataDescriptor"],
+      "amount" : data["amount"],
+      "address_zip" : data["address_zip"]
     }
-  }, 10);
+    console.log(data);
+    console.log(paymentprocessinfo);
+    this.http.post(this.url+"/paymentforconsultaion/5e43a999feb8ea2affebb7f6",paymentprocessinfo)
+      .subscribe(paymentres => {
+      });
+  }
+  else {
+    this.postpaymentdata(data);
+  }
 }
 
 createForm() {
